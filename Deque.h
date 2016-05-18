9fcf8343c6ee520364421fce8f241c78a5c6174d
unknown <ZhengYuXing49.gmail.com>
2016-05-18 09:28:04 +0800

    add List.h,Slist.h,Deque.h

---
+++ Deque.h
@@ -0,0 +1,523 @@
+#ifndef ZYX_DEQUE
+#define ZYX_DEQUE 
+
+#include "Alloc.h"
+#include "Construct.h"
+#include "Algorithm.h"
+#include "Utility.h"
+
+namespace Zyx {
+
+size_t __deque_buf_size(size_t n, size_t sz)
+{
+	return n != 0 ? n : (sz < 512 ? 512 / sz : 1);
+}
+
+template <typename T, typename Ref, typename Ptr, size_t BufSiz = 0>
+struct __deque_iterator
+{
+	typedef __deque_iterator<T, T&, T*, BufSiz> 			   iterator;
+	typedef __deque_iterator<T, const T&, const T*, BufSiz>    const_iterator;
+	typedef __deque_iterator 								   self;
+
+	typedef random_access_iterator_tag    					   iterator_category;
+	typedef T 							  					   value_type;
+	typedef Ptr 						  					   pointer;
+	typedef Ref 						  					   reference;
+	typedef size_t 						  					   size_type;
+	typedef ptrdiff_t					  					   difference_type;
+	typedef T**							  					   map_pointer;
+
+	T* cur;
+	T* first;
+	T* last;
+	map_pointer node;
+
+	__deque_iterator() : cur(nullptr), first(nullptr), last(nullptr), node(nullptr) { }
+	__deque_iterator(const iterator& x) : cur(x.cur), first(x.first), last(x.last), node(x.node) { }
+	__deque_iterator(const const_iterator& x) : cur(x.cur), first(x.first), last(x.last), node(x.node) { }
+
+	static size_type buffer_size() { return __deque_buf_size(BufSiz, sizeof(T)); }
+
+	void set_node(map_pointer new_node)
+	{
+		node = new_node;
+		first = *new_node;
+		last = first + buffer_size();
+	}
+
+	reference operator*() const { return *cur; }
+	pointer operator->() const { return &(operator*()); }
+
+	difference_type operator-(const self& x) const
+	{
+		return (node - x.node - 1) * difference_type(buffer_size()) + (cur - first) + (x.last - x.cur);
+	}
+
+	self& operator++()
+	{
+		++cur;
+		if (cur == last) {
+			set_node(node + 1);
+			cur = first;
+		}
+		return *this;
+	}
+
+	self operator++(int)
+	{
+		self tmp = *this;
+		++*this;
+		return tmp;
+	}
+
+	self& operator--()
+	{
+		if (cur == first) {
+			set_node(node - 1);
+			cur = last;
+		}
+		--cur;
+		return *this;
+	}
+
+	self operator--(int)
+	{
+		self tmp = *this;
+		--*this;
+		return tmp;
+	}
+
+	self& operator+=(difference_type n)
+	{
+		difference_type offset = n + (cur - first);
+		if (offset >= 0 && offset < difference_type(buffer_size())) {
+			cur += n;
+		} else {
+			difference_type node_offset = offset >= 0 ? offset / difference_type(buffer_size()) 
+										  			  : -difference_type((-offset - 1) / buffer_size()) - 1;
+			set_node(node + node_offset);
+			cur = first + (offset - node_offset * difference_type(buffer_size()));
+		}
+		return *this;
+	}
+
+	self operator+(difference_type n) const
+	{
+		self tmp = *this;
+		return tmp += n;
+	}
+
+	self& operator-=(difference_type n) { return *this += -n; }
+
+	self operator-(difference_type n) const
+	{
+		self tmp = *this;
+		return tmp -= n;
+	}
+
+	reference operator[](difference_type n) const { return *(*this + n); }
+
+	bool operator==(const self& x) const { return cur == x.cur; }
+	bool operator!=(const self& x) const { return !(*this == x); }
+
+	bool operator<(const self& x) const
+	{
+		return (node == x.node) ? (cur < x.cur) : (node < x.node);
+	}
+};
+
+template <typename T, typename Alloc = alloc, size_t BufSiz = 0>
+class Deque
+{
+public:
+	template <typename T, typename Alloc, size_t BufSiz>
+	friend bool operator==(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs);
+	template <typename T, typename Alloc, size_t BufSiz>
+	friend bool operator!=(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs);
+	template <typename T, typename Alloc, size_t BufSiz>
+	friend bool operator<(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs);
+	template <typename T, typename Alloc, size_t BufSiz>
+	friend bool operator<=(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs);
+	template <typename T, typename Alloc, size_t BufSiz>
+	friend bool operator>(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs);			
+	template <typename T, typename Alloc, size_t BufSiz>
+	friend bool operator>=(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs);	
+
+public:
+	typedef T 												   value_type;
+	typedef __deque_iterator<T, T&, T*, BufSiz> 			   iterator;
+	typedef __deque_iterator<T, const T&, const T*, BufSiz>    const_iterator;	
+	typedef T* 												   pointer;
+	typedef const T* 										   const_pointer;
+	typedef T& 												   reference;
+	typedef const T& 										   const_reference;
+	typedef size_t 											   size_type;
+	typedef ptrdiff_t 										   difference_type;
+
+private:
+	typedef pointer*						   map_pointer;
+	typedef simple_alloc<value_type, Alloc>    data_allocator;
+	typedef simple_alloc<pointer, Alloc> 	   map_allocator;
+
+public:
+	Deque() { create_map_and_nodes(0); }
+	
+	Deque(size_type n, const T& val) { fill_initialize(n, val); }
+
+	Deque(const Deque& x)
+	{
+		create_map_and_nodes(0);
+		for (const_iterator iter = x.begin(); iter != x.end(); ++iter)
+			push_back(*iter);
+	}
+
+	Deque& operator=(const Deque& x)
+	{
+		if (this != &x) {
+			Deque tmp(x);
+			swap(tmp);
+		}
+		return *this;
+
+		/*
+		if (this != &x) {
+			clear();
+			for (const_iterator iter = x.begin(); iter != x.end(); ++iter)
+				push_back(*iter);
+		}
+		return *this;
+		*/
+	}
+
+	~Deque()
+	{
+		clear();
+		deallocate_node(start.first);
+		map_allocator::deallocate(map, map_size);
+	}
+
+public:
+	iterator begin() { return start; }
+	const_iterator begin() const { return start; }
+
+	iterator end() { return finish; }
+	const_iterator end() const { return finish; }
+
+	bool empty() const { return start == finish; }
+	size_type size() const { return finish - start; }
+	size_type max_size() const { return size_type(-1); }
+
+	reference front() { return *start; }
+	const_reference front() const { return *start; }
+
+	reference back() { return *(finish - 1); }
+	const_reference back() const { return *(finish - 1);  }
+
+	reference operator[](size_type n) { return *(start + n); }
+	const_reference operator[](size_type n) const { return *(start + n); }
+
+public:
+	void push_back(const T& val)
+	{
+		if (finish.cur != finish.last - 1) {
+			construct(finish.cur, val);
+			++finish.cur;
+		} else {
+			push_back_aux(val);
+		}
+	}
+
+	void push_front(const T& val)
+	{
+		if (start.cur != start.first) {
+			construct(start.cur, val);
+			--start.cur;
+		} else {
+			push_front_aux(val);
+		}
+	}
+
+	void pop_back()
+	{
+		if (finish.cur != finish.first) {
+			--finish.cur;
+			destroy(finish.cur);
+		} else {
+			pop_back_aux();
+		}
+	}
+
+	void pop_front()
+	{
+		if (start.cur != start.last - 1) {
+			destroy(start.cur);
+			++start.cur;
+		} else {
+			pop_front_aux();
+		}
+	}
+
+	void clear()
+	{
+		for (map_pointer node = start.node + 1; node < finish.node; ++node) {
+			destroy(*node, *node + buffer_size());
+			deallocate_node(*node);
+		}
+
+		if (start.node != finish.node) {
+			destroy(start.cur, start.last);
+			destroy(finish.first, finish.cur);
+			deallocate_node(finish.first);
+		} else {
+			destroy(start.cur, finish.cur);
+		}
+
+		finish = start;
+	}
+
+	iterator insert(iterator pos, const T& val)
+	{
+		if (pos.cur == start.cur) {
+			push_front(val);
+			return start;
+		} else if (pos.cur == finish.cur) {
+			push_back(val);
+			return finish - 1;
+		} else {
+			return insert_aux(pos, val);
+		}
+	}
+
+	iterator erase(iterator pos)
+	{
+		iterator next = pos + 1;
+		difference_type index = pos - start;
+		if (index < size() / 2) {
+			copy_backward(start, pos, next);
+			pop_front();
+		} else {
+			copy(next, finish, pos);
+			pop_back();
+		}
+		return start + index;
+	}
+
+	iterator erase(iterator first, iterator last)
+	{
+		if (first == start && last == finish) {
+			clear();
+			return finish;
+		}	
+
+		difference_type n = last - first;
+		difference_type elems_before = first - start;
+		if (elems_before < (size() - n) / 2) {
+			copy_backward(start, first, last);
+			iterator new_start = start + n;
+			destroy(start, new_start);
+			for (map_pointer cur = start.node; cur < new_start.node; ++cur)
+				deallocate_node(*cur);
+			start = new_start;
+		} else {
+			copy(last, finish, first);
+			iterator new_finish = finish - n;
+			destroy(new_finish, finish);
+			for (map_pointer cur = new_finish.node + 1; cur <= finish.node; ++cur)
+				deallocate_node(*cur);
+			finish = new_finish;
+		}
+		return start + elems_before;
+	}
+
+	void swap(Deque& x)
+	{
+		Zyx::swap(start, x.start);
+		Zyx::swap(finish, x.finish);
+		Zyx::swap(map, x.map);
+		Zyx::swap(map_size, x.map_size);
+	}
+
+private:
+	static size_type buffer_size() { return __deque_buf_size(BufSiz, sizeof(T)); }
+
+	T* allocate_node() { return data_allocator::allocate(buffer_size()); }
+	void deallocate_node(T* p) { data_allocator::deallocate(p, buffer_size()); }
+
+	void create_map_and_nodes(size_type num_elements)
+	{
+		const size_type initial_map_size = 8;		
+		const size_type num_nodes = num_elements / buffer_size() + 1;
+		map_size = max(initial_map_size, num_nodes + 2);
+		
+		map = map_allocator::allocate(map_size);		
+		map_pointer nstart = map + (map_size - num_nodes) / 2;
+		map_pointer nfinish = nstart + num_nodes - 1;
+
+		for (map_pointer cur = nstart; cur <= nfinish; ++cur)
+			*cur = allocate_node();
+
+		start.set_node(nstart);
+		finish.set_node(nfinish);
+		start.cur = start.first;
+		finish.cur = finish.first + num_elements % buffer_size();
+	}
+
+	void fill_initialize(size_type n, const T& val)
+	{
+		create_map_and_nodes(n);
+		for (map_pointer cur = start.node; cur < finish.node; ++cur)
+			uninitialized_fill(*cur, *cur + buffer_size(), val);
+		uninitialized_fill(finish.first, finish.cur, val);
+	}
+
+	void push_back_aux(const T& val)
+	{
+		reserve_map_at_back();
+		*(finish.node + 1) = allocate_node();
+		construct(finish.cur, val);
+		finish.set_node(finish.node + 1);
+		finish.cur = finish.first;
+	}
+
+	void push_front_aux(const T& val)
+	{
+		reserve_map_at_front();
+		*(start.node - 1) = allocate_node();
+		start.set_node(start.node - 1);
+		start.cur = start.last - 1;
+		construct(start.cur, val);
+	}
+
+	void pop_back_aux()
+	{
+		deallocate_node(finish.first);
+		finish.set_node(finish.node - 1);
+		finish.cur = finish.last - 1;
+		destroy(finish.cur);
+	}
+
+	void pop_front_aux()
+	{
+		destroy(start.cur);
+		deallocate_node(start.first);
+		start.set_node(start.node + 1);
+		start.cur = start.first;
+	}
+
+	iterator insert_aux(iterator pos, const T& val)
+	{
+		difference_type index = pos - start;
+		if (index < size() / 2) {
+			push_front(front());
+			iterator front1 = start + 1;
+			iterator front2 = start + 2;
+			pos = start + index;
+			iterator pos1 = pos + 1;
+			copy(front2, pos1, front1);
+		} else {
+			push_back(back());
+			iterator back1 = finish - 1;
+			iterator back2 = finish - 2;
+			pos = start + index;
+			copy_backward(pos, back2, back1);
+		}
+		*pos = val;
+		return pos;	
+	}
+
+private:
+	void reserve_map_at_back(size_type nodes_to_add = 1)
+	{
+		if (map_size - (finish.node - map) < nodes_to_add + 1)
+			reallocate_map(nodes_to_add, false);
+	}
+
+	void reserve_map_at_front(size_type nodes_to_add = 1)
+	{
+		if (start.node - map < nodes_to_add)
+			reallocate_map(nodes_to_add, true);
+	}
+
+	void reallocate_map(size_type nodes_to_add, bool add_at_front)
+	{
+		size_type old_num_nodes = finish.node - start.node + 1;
+		size_type new_num_nodes = old_num_nodes + nodes_to_add;
+		map_pointer new_start;
+		if (map_size > 2 * new_num_nodes) {
+			new_start = map + (map_size - new_num_nodes) / 2 + (add_at_front ? nodes_to_add : 0);
+			if (new_start < start.node)
+				copy(start.node, finish.node + 1, new_start);
+			else
+				copy_backward(start.node, finish.node + 1, new_start + old_num_nodes);
+		} else {
+			size_type new_map_size = map_size + max(map_size, nodes_to_add) + 2;
+			map_pointer new_map = map_allocator::allocate(new_map_size);
+			new_start = new_map + (new_map_size - new_num_nodes) / 2 + (add_at_front ? nodes_to_add : 0);
+			copy(start.node, finish.node + 1, new_start);
+			map_allocator::deallocate(map, map_size);
+			map = new_map;
+			map_size = new_map_size;
+		}
+		start.set_node(new_start);
+		finish.set_node(new_start + old_num_nodes - 1);
+	}
+
+private:
+	iterator start;
+	iterator finish;	
+	map_pointer map;
+	size_type map_size;
+};
+
+template <typename T, typename Alloc, size_t BufSiz>
+bool operator==(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs)
+{
+	if (lhs.size() != rhs.size())
+		return false;
+	
+	for (Deque<T, Alloc, BufSiz>::size_type i = 0; i < lhs.size(); i++)
+		if (lhs[i] != rhs[i])
+			return false;
+	return true;
+}
+
+template <typename T, typename Alloc, size_t BufSiz>
+bool operator!=(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs)
+{
+	return !(lhs == rhs);
+}
+
+template <typename T, typename Alloc, size_t BufSiz>
+bool operator<(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs)
+{
+
+}
+
+template <typename T, typename Alloc, size_t BufSiz>
+bool operator<=(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs)
+{
+
+}
+
+template <typename T, typename Alloc, size_t BufSiz>
+bool operator>(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs)	
+{
+
+}		
+
+template <typename T, typename Alloc, size_t BufSiz>
+bool operator>=(const Deque<T, Alloc, BufSiz>& lhs, const Deque<T, Alloc, BufSiz>& rhs)	
+{
+
+}
+
+template <typename T, typename Alloc, size_t BufSiz>
+void swap(Deque<T, Alloc, BufSiz>& x, Deque<T, Alloc, BufSiz>& y)
+{
+	x.swap(y);
+}
+
+}
+
+#endif
---
+++ List.h
@@ -0,0 +1,404 @@
+#ifndef ZYX_LIST
+#define ZYX_LIST 
+
+#include "Iterator.h"
+#include "Alloc.h"
+#include "Construct.h"
+#include "Utility.h"
+
+namespace Zyx {
+
+template <typename T>
+struct __list_node
+{
+	T data;	
+	__list_node* prev;
+	__list_node* next;
+};
+
+template <typename T, typename Ref, typename Ptr>
+struct __list_iterator  
+{
+	typedef __list_iterator<T, Ref, Ptr> 		      self;
+	typedef __list_iterator<T, T&, T*>      		  iterator;
+	typedef __list_iterator<T, const T&, const T*>    const_iterator;
+
+	typedef bidirectional_iterator_tag      		  iterator_category;
+	typedef T 						       			  value_type;
+	typedef Ptr 					        		  pointer;
+	typedef Ref 					        		  reference;
+	typedef size_t 					        		  size_type;
+	typedef ptrdiff_t 				        		  difference_type;
+	typedef __list_node<T>* 		        		  link_type;
+ 
+	link_type node;
+
+	__list_iterator() : node(nullptr) { }
+	__list_iterator(link_type x) : node(x) { }
+	__list_iterator(const iterator& x) : node(x.node) { }
+	__list_iterator(const const_iterator& x) : node(x.node) { }
+
+	bool operator==(const self& x) const { return node == x.node; }
+	bool operator!=(const self& x) const { return node != x.node; }
+
+	reference operator*() const { return node->data; }
+	pointer operator->() const { return &(operator*()); }
+
+	self& operator++()
+	{
+		node = node->next;
+		return *this;
+	}
+
+	self operator++(int)
+	{
+		self tmp = *this;
+		++*this;
+		return tmp;
+	}
+
+	self& operator--()
+	{
+		node = node->prev;
+		return *this;
+	}
+
+	self operator--(int)
+	{
+		self tmp = *this;
+		--*this;
+		return tmp;
+	}
+};
+
+template <typename T, typename Alloc = alloc>
+class List
+{
+public:
+	friend bool operator==(const List& lhs, const List& rhs);
+	friend bool operator!=(const List& lhs, const List& rhs);
+	friend bool operator<(const List& lhs, const List& rhs);
+	friend bool operator<=(const List& lhs, const List& rhs);
+	friend bool operator>(const List& lhs, const List& rhs);
+	friend bool operator>=(const List& lhs, const List& rhs);
+
+private:
+	typedef __list_node<T> 				      list_node;
+	typedef simple_alloc<list_node, Alloc>    list_node_allocator;
+
+public:
+	typedef T 										  value_type;
+	typedef __list_iterator<T, T&, T*> 				  iterator;
+	typedef __list_iterator<T, const T&, const T*>    const_iterator;
+	typedef T*										  pointer;
+	typedef const T* 								  const_pointer;
+	typedef T& 										  reference;
+	typedef const T& 								  const_reference;
+	typedef size_t 									  size_type;
+	typedef ptrdiff_t 								  difference_type;
+	typedef list_node* 								  link_type;
+
+public:
+	List() { empty_intialize(); }
+
+	/* failed!!!
+	explicit List(size_type n, const T& val = T())
+	{
+		empty_intialize();
+		for (int i = 0; i < n; i++)
+			push_back(val);
+	}
+	*/
+
+	template <typename InputIterator>
+	List(InputIterator first, InputIterator last)
+	{
+		empty_intialize();
+		while (first != last) {
+			push_back(*first);
+			++first;
+		}
+	}
+
+	List(const List& x)
+	{
+		empty_intialize();
+		for (const_iterator iter = x.begin(); iter != x.end(); ++iter)
+			push_back(*iter);
+	}
+
+	List& operator=(const List& x)
+	{
+		if (this != &x) {
+			List tmp(x);
+			swap(tmp);
+		}
+		return *this;
+
+		/*
+		if (this != &x) {
+			clear();
+			for (const_iterator iter = x.begin(); iter != x.end(); ++iter)
+				push_back(*iter);
+		}
+		return *this;
+		*/
+	}
+
+	~List() 
+	{
+		clear();
+		destroy_node(node);
+	}
+
+public:
+	iterator begin() { return node->next; }
+	const_iterator begin() const { return node->next; }  
+
+	iterator end() { return node; }
+	const_iterator end() const { return node; }   
+
+	bool empty() const { return node->next == node; }
+	size_type size() const { return distance(begin(), end()); }
+
+	reference front() { return *begin(); }
+	const_reference front() const { return *begin(); }
+
+	reference back() { return *(--end()); }
+	const_reference back() const { return *(--end()); }
+
+public:
+	void push_front(const T& val) { insert(begin(), val); }
+	void push_back(const T& val) { insert(end(), val); }
+
+	void pop_front() { erase(begin()); }
+	void pop_back() { erase(--end()); }
+
+	iterator insert(iterator pos, const T& val)
+	{
+ 		link_type tmp = create_node(val);
+ 		tmp->next = pos.node;
+ 		tmp->prev = pos.node->prev;
+ 		pos.node->prev->next = tmp;
+ 		pos.node->prev = tmp;
+ 		return tmp;
+	}
+
+	iterator erase(iterator pos)
+	{
+		link_type prev_node = pos.node->prev;
+		link_type next_node = pos.node->next;
+		prev_node->next = next_node;
+		next_node->prev = prev_node;
+		destroy_node(pos.node);
+		return next_node;
+	}
+
+	void clear()
+	{
+		link_type cur = node->next;
+		while (cur != node) {
+			link_type tmp = cur;
+			cur = cur->next;
+			destroy_node(tmp);
+		}
+		node->next = node;
+		node->prev = node;
+	}
+
+	void swap(List& x)
+	{
+		Zyx::swap(node, x.node);
+	}
+
+public:
+	void remove(const T& val)
+	{
+		iterator first = begin();
+		iterator last = end();
+		while (first != last) {
+			iterator tmp = first;
+			++tmp;
+			if (*first == val)
+				erase(first);
+			first = tmp;
+		}
+	}
+
+	template <typename Predicate>
+	void remove_if(Predicate pred)
+	{
+		iterator first = begin();
+		iterator last = end();
+		while (first != last) {
+			iterator tmp = first;
+			++tmp;
+			if (pred(*first))
+				erase(first);
+			first = tmp;
+		}
+	}
+
+	void unique()
+	{
+		iterator first = begin();
+		iterator last = end();
+
+		if (first == last)
+			return;
+
+		iterator cur = first;
+		while (++cur != last) {
+			if (*cur == *first) 
+				erase(cur);
+			else 
+				first = cur;                 
+			cur = first;
+		}
+	}
+
+	void splice(iterator pos, List& x)
+	{
+		if (!x.empty())
+			transfer(pos, x.begin(), x.end());
+	}
+
+	void splice(iterator pos, List& x, iterator i)
+	{
+		iterator j = i;
+		++j;
+		if (pos == i || pos == j)
+			return;
+		transfer(pos, i, j);
+	}
+
+	void splice(iterator pos, List& x, iterator first, iterator last)
+	{
+		if (first != last)
+			transfer(pos, first, last);
+	}
+
+	void merge(List& x)
+	{
+		iterator first1 = begin();
+		iterator last1 = end();
+		iterator first2 = x.begin();
+		iterator last2 = x.end();
+
+		while (first1 != last1 && first2 != last2) {
+			if (*first2 < *first1) {
+				iterator next = first2;
+				transfer(first1, first2, ++next);
+				first2 = next;
+			} else {
+				++first1;
+			}
+		}
+
+		if (first2 != last2)
+			transfer(last1, first2, last2);
+	}
+
+	void reverse()
+	{
+		if (node->next == node || node->next->next == node)
+			return;
+
+		iterator first = begin();
+		++first;
+		while (first != end()) {
+			iterator old = first;
+			++first;
+			transfer(begin(), old, first);
+		}
+
+		/* 
+		// another way:		
+		link_type prev_node = node->next;
+		link_type curr_node = node->next->next;
+		while (curr_node != node) {
+			prev_node->next = curr_node->next;
+			curr_node->next = node->next;
+			node->next = curr_node;
+			prev_node->next->prev = prev_node;
+			curr_node->next->prev = curr_node;
+			curr_node->prev = node;
+			curr_node = prev_node->next;
+		}
+		*/
+	}
+
+	void sort()
+	{
+		if (node->next == node || node->next->next == node)
+			return;
+
+		List carry;
+		List counter[64];
+		int fill = 0;
+		while (!empty()) {
+			carry.splice(carry.begin(), *this, begin());
+			int i = 0;
+			while (i < fill && !counter[i].empty()) {
+				counter[i].merge(carry);
+				carry.swap(counter[i++]);
+			}
+			carry.swap(counter[i]);
+			if (i == fill)
+				++fill;
+		}
+
+		for (int i = 1; i < fill; i++) 
+			counter[i].merge(counter[i-1]);
+		swap(counter[fill-1]);
+	}
+
+private:
+	link_type get_node() { return list_node_allocator::allocate(); }
+	
+	void put_node(link_type p) { list_node_allocator::deallocate(p); }
+
+	link_type create_node(const T& x)
+	{
+		link_type p = get_node();
+		construct(&p->data, x);
+		return p;
+	}
+
+	void destroy_node(link_type p)
+	{
+		destroy(&p->data);
+		put_node(p);
+	}
+
+	void empty_intialize()
+	{
+		node = get_node();
+		node->next = node;
+		node->prev = node;
+	}
+
+	void transfer(iterator pos, iterator first, iterator last)
+	{
+		last.node->prev->next = pos.node;
+		first.node->prev->next = last.node;
+		pos.node->prev->next = first.node;
+		link_type tmp = pos.node->prev;
+		pos.node->prev = last.node->prev;
+		last.node->prev = first.node->prev;
+		first.node->prev = tmp;
+	}
+
+private:
+	link_type node;
+};
+
+template <typename T, typename Alloc>
+void swap(List<T, Alloc>& x, List<T, Alloc>& y)
+{
+	x.swap(y);
+}
+
+}
+
+#endif
