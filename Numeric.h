a8c4c5c2348fe29d15c735509229d7731b957f55
unknown <ZhengYuXing49.gmail.com>
2016-05-21 20:31:17 +0800

    add Utility.h and Numeric.h

---
+++ Numeric.h
@@ -0,0 +1,30 @@
+#ifndef ZYX_NUMERIC
+#define ZYX_NUMERIC 
+
+namespace Zyx {
+
+//---------------------------------【accumulate() function】-----------------------------------
+
+template <typename InputIterator, typename T>
+T accumulate(InputIterator first, InputIterator last, T init)
+{
+	while (first != last) {
+		init = init + *first;
+		++first;
+	}
+	return init;
+}
+
+template <typename InputIterator, typename T, typename BinaryOperation>
+T accumulate(InputIterator first, InputIterator last, T init, BinaryOperation binary_op)
+{
+	while (first != last) {
+		init = binary_op(init, *first);
+		++first;
+	}
+	return init;
+}
+
+}
+
+#endif
---
+++ Utility.h
@@ -0,0 +1,22 @@
+#ifndef ZYX_UTILITY
+#define ZYX_UTILITY 
+
+namespace Zyx {
+
+template <typename T>
+void swap(T& a, T& b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename ForwardIterator1, typename ForwardIterator2>
+void iter_swap(ForwardIterator1 a, ForwardIterator2 b)
+{
+	swap(*a, *b);
+}
+
+}
+
+#endif
