785120a12b53b2b52f75ae625e95098c460cc437
unknown <ZhengYuXing49.gmail.com>
2016-05-24 16:24:41 +0800

    add Pair

--- Utility.h
+++ Utility.h
@@ -17,6 +17,96 @@ void iter_swap(ForwardIterator1 a, ForwardIterator2 b)
     swap(*a, *b);
 }
 
+template <typename T1, typename T2>
+struct Pair
+{
+    typedef T1 first_type;
+    typedef T2 second_type;
+
+    T1 first;
+    T2 second;
+
+    Pair() { }
+
+    template <typename U, typename V>
+    Pair(const Pair<U, V>& pr) : first(pr.first), second(pr.second) { }
+
+    Pair(const first_type& a, const second_type& b) : first(a), second(b) { }
+
+    Pair& operator=(const Pair& pr)
+    {
+        if (this != &pr) {
+            first = pr.first;
+            second = pr.second;
+        }
+        return *this;
+    }
+
+    template <typename U, typename V>
+    Pair& operator=(const Pair<U, V>& pr)
+    {
+        if (this != &pr) {
+            first = pr.first;
+            second = pr.second;
+        }
+        return *this;   	
+    }
+
+    void swap(Pair& pr)
+    {
+        Zyx::swap(first, pr.first);
+        Zyx::swap(second, pr.second);
+    }
+};
+
+template <typename T1, typename T2>
+bool operator==(const Pair<T1, T2>& lhs, const Pair<T1, T2>& rhs)
+{
+    return lhs.first == rhs.first && lhs.second == rhs.second;
+}
+
+template <typename T1, typename T2>
+bool operator!=(const Pair<T1, T2>& lhs, const Pair<T1, T2>& rhs)
+{
+    return !(lhs == rhs);
+}
+
+template <typename T1, typename T2>
+bool operator<(const Pair<T1, T2>& lhs, const Pair<T1, T2>& rhs)
+{
+    return lhs.first < rhs.first || (!(lhs.first < rhs.first) && lhs.second < rhs.second);
+}
+
+template <typename T1, typename T2>
+bool operator<=(const Pair<T1, T2>& lhs, const Pair<T1, T2>& rhs)
+{
+    return !(rhs < lhs);
+}
+
+template <typename T1, typename T2>
+bool operator>(const Pair<T1, T2>& lhs, const Pair<T1, T2>& rhs)
+{
+    return rhs < lhs;
+}
+
+template <typename T1, typename T2>
+bool operator>=(const Pair<T1, T2>& lhs, const Pair<T1, T2>& rhs)
+{
+    return !(lhs < rhs);
+}
+
+template <typename T1, typename T2>
+void swap(Pair<T1, T2>& x, Pair<T1, T2>& y)
+{
+    x.swap(y);
+}
+
+template <typename T1, typename T2>
+Pair<T1, T2> make_pair(const T1& x, const T2& y)
+{
+	return Pair<T1, T2>(x, y);
+}
+
 }
 
 #endif
