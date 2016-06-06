46d2007279cb85eebdcf7a720b9043ecee58ba88
unknown <ZhengYuXing49.gmail.com>
2016-06-06 23:00:03 +0800

    add some functions

--- Algorithm.h
+++ Algorithm.h
@@ -1,9 +1,23 @@
 #ifndef ZYX_ALGORITHM
 #define ZYX_ALGORITHM 
 
+#include <cstdlib>
 #include "Iterator.h"
+#include "Utility.h"
 
 namespace Zyx {
+
+//----------------------------------【for_each() function】-----------------------------------
+
+template <typename InputIterator, typename Function>
+Function for_each(InputIterator first, InputIterator last, Function fn)
+{
+    while (first != last) {
+        fn(*first);
+        ++first;
+    }
+    return fn;
+}
 
 //-------------------------------------【max() function】-------------------------------------
 
@@ -402,16 +416,517 @@ OutputIterator remove_copy_if(InputIterator first, InputIterator last,
     return result;
 }
 
-//----------------------------------【for_each() function】-----------------------------------
+//----------------------------------【reverse() function】------------------------------------
 
-template <typename InputIterator, typename Function>
-Function for_each(InputIterator first, InputIterator last, Function fn)
+template <typename BidirectionalIterator>
+void reverse(BidirectionalIterator first, BidirectionalIterator last)
 {
-    while (first != last) {
-        fn(*first);
+    __reverse(first, last, iterator_category(first));
+}
+
+template <typename BidirectionalIterator>
+void __reverse(BidirectionalIterator first, BidirectionalIterator last, 
+               bidirectional_iterator_tag)
+{
+    while (first != last && first != --last) {
+        iter_swap(first, last);
         ++first;
     }
-    return fn;
+}
+
+template <typename RandomAccessIterator>
+void __reverse(RandomAccessIterator first, RandomAccessIterator last, 
+               random_access_iterator_tag)
+{
+    while (first < last) 
+        iter_swap(first++, --last);
+}
+
+//--------------------------------【reverse_copy() function】---------------------------------
+
+template <typename BidirectionalIterator, typename OutputIterator>
+OutputIterator reverse_copy(BidirectionalIterator first, BidirectionalIterator last, 
+                            OutputIterator result)
+{
+    while (first != last) {
+        --last;
+        *result = *last;
+        ++result;
+    }
+    return result;
+}
+
+//--------------------------------【lower_bound() function】----------------------------------
+
+template <typename ForwardIterator, typename T>
+ForwardIterator lower_bound(ForwardIterator first, ForwardIterator last, const T& val)
+{
+    return __lower_bound(first, last, val, difference_type(first), iterator_category(first));
+}
+
+template <typename ForwardIterator, typename T, typename Distance>
+ForwardIterator __lower_bound(ForwardIterator first, ForwardIterator last, const T& val, 
+                              Distance*, forward_iterator_tag)
+{
+    Distance len = distance(first, last);
+    while (len > 0) {
+        Distance half = len >> 1;
+        ForwardIterator middle = first;
+        advance(middle, half);
+        if (*middle < val) {
+            first = ++middle;
+            len = len - half - 1;
+        } else {
+            len = half;
+        }
+    }
+    return first;
+}
+
+template <typename RandomAccessIterator, typename T, typename Distance>
+RandomAccessIterator __lower_bound(RandomAccessIterator first, RandomAccessIterator last, 
+                                   const T& val, Distance*, random_access_iterator_tag)
+{
+    Distance len = last - first;
+    while (len > 0) {
+        Distance half = len >> 1;
+        RandomAccessIterator middle = first + half;
+        if (*middle < val) {
+            first = middle + 1;
+            len = len - half - 1;
+        } else {
+            len = half;
+        }
+    }
+    return first;
+}
+
+template <typename ForwardIterator, typename T, typename Compare>
+ForwardIterator lower_bound(ForwardIterator first, ForwardIterator last, const T& val, Compare comp)
+{
+    typedef iterator_traits<ForwardIterator>::difference_type difference_type; 
+    difference_type len = distance(first, last);
+    while (len > 0) {
+        difference_type half = len / 2;
+        ForwardIterator middle = first;
+        advance(middle, half);
+        if (comp(*middle, val)) {
+            first = ++middle;
+            len = len - half - 1;
+        } else {
+            len = half;
+        }
+    }
+    return first;
+}
+
+//--------------------------------【upper_bound() function】----------------------------------
+
+template <typename ForwardIterator, typename T>
+ForwardIterator upper_bound(ForwardIterator first, ForwardIterator last, const T& val)
+{
+    return __upper_bound(first, last, val, difference_type(first), iterator_category(first));
+}
+
+template <typename ForwardIterator, typename T, typename Distance>
+ForwardIterator __upper_bound(ForwardIterator first, ForwardIterator last, const T& val,
+                              Distance*, forward_iterator_tag)
+{
+    Distance len = distance(first, last);
+    while (len > 0) {
+        Distance half = len >> 1;
+        ForwardIterator middle = first;
+        advance(middle, half);
+        if (val < *middle) {
+            len = half;
+        } else {
+            first = ++middle;
+            len = len - half - 1;
+        }
+    }
+    return first;
+}
+
+template <typename RandomAccessIterator, typename T, typename Distance>
+RandomAccessIterator __upper_bound(RandomAccessIterator first, RandomAccessIterator last, 
+                                   const T& val, Distance*, random_access_iterator_tag)
+{
+    Distance len = last - first;
+    while (len > 0) {
+        Distance half = len >> 1;
+        RandomAccessIterator middle = first + half;
+        if (val < *middle) {
+            len = half;
+        } else {
+            first = middle + 1;
+            len = len - half - 1;
+        }
+    }
+    return first;
+}
+
+template <typename ForwardIterator, typename T, typename Compare>
+ForwardIterator upper_bound(ForwardIterator first, ForwardIterator last, const T& val, Compare comp)
+{
+    typedef iterator_traits<ForwardIterator>::difference_type difference_type; 
+    difference_type len = distance(first, last);
+    while (len > 0) {
+        difference_type half = len / 2;
+        ForwardIterator middle = first;
+        advance(middle, half);
+        if (comp(val, *middle)) {
+            len = half;            
+        } else {
+            first = ++middle;
+            len = len - half - 1;
+        }
+    }
+    return first;
+}
+
+//-------------------------------【binary_search() function】---------------------------------
+
+template <typename ForwardIterator, typename T>
+bool binary_search(ForwardIterator first, ForwardIterator last, const T& val)
+{
+    ForwardIterator iter = lower_bound(first, last, val);
+    return iter != last && !(val < *iter);
+}
+
+template <typename ForwardIterator, typename T, typename Compare>
+bool binary_search(ForwardIterator first, ForwardIterator last, const T& val, Compare comp)
+{
+    ForwardIterator iter = lower_bound(first, last, val);
+    return iter != last && !comp(val, *iter);
+}
+
+//--------------------------------【equal_range() function】----------------------------------
+
+template <typename ForwardIterator, typename T>
+Pair<ForwardIterator, ForwardIterator> 
+equal_range(ForwardIterator first, ForwardIterator last, const T& val)
+{
+    ForwardIterator iter = lower_bound(first, last, val);
+    return make_pair(iter, upper_bound(iter, last, val));
+}
+
+template <typename ForwardIterator, typename T, typename Compare>
+Pair<ForwardIterator, ForwardIterator> 
+equal_range(ForwardIterator first, ForwardIterator last, const T& val, Compare comp)
+{
+    ForwardIterator iter = lower_bound(first, last, val, comp);
+    return make_pair(iter, upper_bound(iter, last, val, comp));
+}
+
+//-------------------------------【next_permutation() function】------------------------------
+
+template <typename BidirectionalIterator>
+bool next_permutation(BidirectionalIterator first, BidirectionalIterator last)
+{
+    if (first == last)
+        return false;
+
+    BidirectionalIterator i = first;
+    if (i == last)
+        return false;
+
+    i = last;
+    --i;
+    while (true) {
+        BidirectionalIterator ii = i;
+        --i;
+        if (*i < *ii) {
+            BidirectionalIterator j = last;
+            while (!(*i < *--j))
+                ;
+            iter_swap(i, j);
+            reverse(ii, last);
+            return true;
+        }
+        if (i == first) {
+            reverse(first, last);
+            return false;
+        }
+    }
+
+    /* another way:
+    if (first == last)
+        return false;
+
+    BidirectionalIterator i = first;
+    if (i == last)
+        return false;
+
+    i = last;
+    --i;  
+    BidirectionalIterator ii = i;
+    --i;  
+    while (i != first && !(*i < *ii)) {
+        --i;
+        --ii;
+    }
+
+    if (i == first && !(*i < *ii)) {
+        reverse(first, last);
+        return false;
+    }
+
+    BidirectionalIterator j = last;
+    while (!(*i < *--j))
+        ;
+    iter_swap(i, j);
+    reverse(ii, last);
+    return true;
+    */
+}
+
+//-------------------------------【prev_permutation() function】------------------------------
+
+template <typename BidirectionalIterator>
+bool prev_permutation(BidirectionalIterator first, BidirectionalIterator last)
+{
+    if (first == last)
+        return false;
+
+    BidirectionalIterator i = first;
+    ++i;
+    if (i == last)
+        return false;
+
+    i = last;
+    --i;
+    while (true) {
+        BidirectionalIterator ii = i;
+        --i;        
+        if (*ii < *i) {
+            BidirectionalIterator j = last;
+            while (!(*--j < *i))
+                ;
+            iter_swap(i, j);
+            reverse(ii, last);
+            return true;
+        }
+        if (i == first) {
+            reverse(first, last);
+            return false;
+        }
+    }
+}
+
+//--------------------------------【random_shuffle() function】-------------------------------
+
+template <typename RandomAccessIterator>
+void random_shuffle(RandomAccessIterator first, RandomAccessIterator last)
+{
+    __random_shuffle(first, last, difference_type(first));
+}
+
+template <typename RandomAccessIterator, typename Distance>
+void __random_shuffle(RandomAccessIterator first, RandomAccessIterator last, Distance*)
+{
+    if (first == last)
+        return;
+    for (RandomAccessIterator i = first + 1; i != last; ++i)
+        iter_swap(i, first + Distance(rand() % (i - first) + 1));
+}
+
+template <typename RandomAccessIterator, typename RandomNumberGenerator>
+void random_shuffle(RandomAccessIterator first, RandomAccessIterator last, 
+                    RandomNumberGenerator &rand)
+{
+    if (first == last)
+        return;
+    for (RandomAccessIterator i = first + 1; i != last; ++i)
+        iter_swap(i, first + rand((i - first) + 1));
+}
+
+//---------------------------------【partial_sort() function】--------------------------------
+
+template <typename RandomAccessIterator>
+void partial_sort(RandomAccessIterator first, RandomAccessIterator middle, 
+                  RandomAccessIterator last)
+{
+    __partial_sort(first, middle, last, value_type(first));
+}
+
+template <typename RandomAccessIterator, typename T>
+void __partial_sort(RandomAccessIterator first, RandomAccessIterator middle,
+                    RandomAccessIterator last, T*)
+{
+    make_heap(first, middle);
+    for (RandomAccessIterator iter = middle; iter < last; ++iter)
+        if (*iter < *first)
+            __pop_heap(first, middle, iter, T(*iter), difference_type(first));
+    sort_heap(first, middle);
+}
+
+template <typename InputIterator, typename RandomAccessIterator>
+RandomAccessIterator partial_sort_copy(InputIterator first, InputIterator last,
+                                       RandomAccessIterator result_first,
+                                       RandomAccessIterator result_last)
+{
+    
+} 
+
+//-------------------------------------【sort() function】------------------------------------
+
+template <typename RandomAccessIterator>
+void sort(RandomAccessIterator first, RandomAccessIterator last)
+{
+    if (first != last) {
+        __introsort_loop(first, last, value_type(first), __lg(last - first) * 2);
+        __final_insertion_sort(first, last);
+    }
+}
+
+// another way
+template <typename RandomAccessIterator>
+void quick_sort(RandomAccessIterator first, RandomAccessIterator last)
+{
+    if (first != last) {
+        __quick_sort_loop(first, last);
+        __final_insertion_sort(first, last);
+    }
+}
+
+template <typename RandomAccessIterator>
+void __quick_sort_loop(RandomAccessIterator first, RandomAccessIterator last)
+{
+    __quick_sort_loop_aux(first, last, value_type(first));
+}
+
+template <typename RandomAccessIterator, typename T>
+void __quick_sort_loop_aux(RandomAccessIterator first, RandomAccessIterator last, T*)
+{
+    while (last - first > __stl_threshold) {
+        RandomAccessIterator cut = __unguarded_partition(first, last, T(__median(*first, 
+                                                         *(first + (last - first)/2), *(last - 1))));
+        if (cut - first >= last - cut) {
+            __quick_sort_loop(cut, last);
+            last = cut;
+        } else {
+            __quick_sort_loop(first, cut);
+            first = cut;
+        }
+    }
+}
+
+const int __stl_threshold = 16;
+
+template <typename Size>
+Size __lg(Size n)
+{
+    Size k;
+    for (k = 0; n > 1; n >>= 1)
+        ++k;
+    return k;
+}
+
+template <typename T>
+const T& __median(const T& a, const T& b, const T& c)
+{
+    if (a < b)
+        if (b < c)
+            return b;
+        else if (a < c)
+            return c;
+        else
+            return a;
+    else if (a < c)
+        return a;
+    else if (b < c)
+        return c;
+    else
+        return b;
+}
+
+template <typename RandomAccessIterator, typename T, typename Size>
+void __introsort_loop(RandomAccessIterator first, RandomAccessIterator last, T*, Size depth_limit)
+{
+    while (last - first > __stl_threshold) {
+        if (depth_limit == 0) {
+            partial_sort(first, last, last);
+            return;
+        }
+        --depth_limit;
+        RandomAccessIterator cut = __unguarded_partition(first, last, T(__median(*first, 
+                                                         *(first + (last - first)/2), *(last - 1))));
+        __introsort_loop(cut, last, value_type(first), depth_limit);
+        last = cut;
+    }
+}
+
+template <typename RandomAccessIterator, typename T>
+RandomAccessIterator __unguarded_partition(RandomAccessIterator first, RandomAccessIterator last, T pivot)
+{
+    while (true) {
+        while (*first < pivot) 
+            ++first;
+        --last;
+        while (pivot < *last)
+            --last;
+        if (!(first < last))
+            return first;
+        iter_swap(first, last);
+        ++first;
+    }
+}
+
+template <typename RandomAccessIterator>
+void __final_insertion_sort(RandomAccessIterator first, RandomAccessIterator last)
+{
+    if (last - first > __stl_threshold) {
+        __insertion_sort(first, first + __stl_threshold);
+        __unguarded_insertion_sort(first + __stl_threshold, last);
+    } else {
+        __insertion_sort(first, last);
+    }
+}
+
+template <typename RandomAccessIterator>
+void __insertion_sort(RandomAccessIterator first, RandomAccessIterator last)
+{
+    if (first == last)
+        return;
+    for (RandomAccessIterator iter = first + 1; iter != last; ++iter)
+        __linear_insert(first, iter, value_type(first));
+}
+
+template <typename RandomAccessIterator, typename T>
+void __linear_insert(RandomAccessIterator first, RandomAccessIterator last, T*)
+{
+    T value = *last;
+    if (value < *first) {
+        copy_backward(first, last, last + 1);
+        *first = value;
+    } else {
+        __unguarded_linear_insert(last, value);
+    }
+}
+
+template <typename RandomAccessIterator, typename T>
+void __unguarded_linear_insert(RandomAccessIterator last, T value)
+{
+    RandomAccessIterator next = last - 1;
+    while (value < *next) {
+        *last = *next;
+        last = next;
+        --next;
+    }
+    *last = value;
+}
+
+template <typename RandomAccessIterator>
+void __unguarded_insertion_sort(RandomAccessIterator first, RandomAccessIterator last)
+{
+    __unguarded_insertion_sort_aux(first, last, value_type(first));
+}
+
+template <typename RandomAccessIterator, typename T>
+void __unguarded_insertion_sort_aux(RandomAccessIterator first, RandomAccessIterator last, T*)
+{
+    for (RandomAccessIterator iter = first; iter != last; ++iter)
+        __unguarded_linear_insert(iter, T(*iter));
 }
 
 //-------------------------------【perlocate_down() function】--------------------------------
@@ -540,7 +1055,6 @@ void __make_heap(RandomAccessIterator first, RandomAccessIterator last, T*, Dist
 {
     if (last - first < 2)
         return;
-
     Distance len = last - first;
     Distance parent = (len - 2) / 2;
     while (parent >= 0) {
