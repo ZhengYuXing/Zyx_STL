4efa3aa9f4edd01587c781d6623ae2e3dac4028b
unknown <ZhengYuXing49.gmail.com>
2016-07-24 18:50:29 +0800

    change format

--- Numeric.h
+++ Numeric.h
@@ -8,21 +8,21 @@ namespace Zyx {
 template <typename InputIterator, typename T>
 T accumulate(InputIterator first, InputIterator last, T init)
 {
-	while (first != last) {
-		init = init + *first;
-		++first;
-	}
-	return init;
+    while (first != last) {
+        init = init + *first;
+        ++first;
+    }
+    return init;
 }
 
 template <typename InputIterator, typename T, typename BinaryOperation>
 T accumulate(InputIterator first, InputIterator last, T init, BinaryOperation binary_op)
 {
-	while (first != last) {
-		init = binary_op(init, *first);
-		++first;
-	}
-	return init;
+    while (first != last) {
+        init = binary_op(init, *first);
+        ++first;
+    }
+    return init;
 }
 
 }
