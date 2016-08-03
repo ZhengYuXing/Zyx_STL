184b0d927b3181e31d37b6eb603c4c08f75ff062
ZhengYuXing <zhengyuxing49@gmail.com>
2016-08-03 16:09:54 +0800

    add includes() function

--- Algorithm.h
+++ Algorithm.h
@@ -403,6 +403,42 @@ ForwardIterator search_n(ForwardIterator first, ForwardIterator last,
     return last;	
 }
 
+//----------------------------------【includes() function】-----------------------------------
+
+template <typename InputIterator1, typename InputIterator2>
+bool includes(InputIterator1 first1, InputIterator1 last1, 
+              InputIterator2 first2, InputIterator2 last2)
+{
+    while (first1 != last1 && first2 != last2) {
+        if (*first2 < *first1) {
+            return false;
+        } else if (*first1 < *first2) {
+            ++first1;
+        } else {
+            ++first1;
+            ++first2;
+        }
+    }
+    return first2 == last2;
+}
+
+template <typename InputIterator1, typename InputIterator2, typename Compare>
+bool includes(InputIterator1 first1, InputIterator1 last1, 
+              InputIterator2 first2, InputIterator2 last2, Compare comp)
+{
+    while (first1 != last1 && first2 != last2) {
+        if (comp(*first2, *first1)) {
+            return false;
+        } else if (comp(*first1, *first2)) {
+            ++first1;
+        } else {
+            ++first1;
+            ++first2;
+        }
+    }
+    return first2 == last2;
+}
+
 //-------------------------------------【copy() function】------------------------------------
 
 template <typename InputIterator, typename OutputIterator>
@@ -844,13 +880,31 @@ OutputIterator merge(InputIterator1 first1, InputIterator1 last1,
     while (first1 != last1 && first2 != last2) {
         if (*first2 < *first1) {
             *result = *first2;
-            ++first2;
-            ++result;            
+            ++first2;         
         } else {
             *result = *first1;
             ++first1;
-            ++result;
         }
+        ++result;
+    }
+    return copy(first2, last2, copy(first1, last1, result));    
+}
+
+template <typename InputIterator1, typename InputIterator2, 
+          typename OutputIterator, typename Compare>
+OutputIterator merge(InputIterator1 first1, InputIterator1 last1, 
+                     InputIterator2 first2, InputIterator2 last2, 
+                     OutputIterator result, Compare comp)
+{
+    while (first1 != last1 && first2 != last2) {
+        if (comp(*first2, *first1)) {
+            *result = *first2;
+            ++first2;         
+        } else {
+            *result = *first1;
+            ++first1;
+        }
+        ++result;
     }
     return copy(first2, last2, copy(first1, last1, result));    
 }
