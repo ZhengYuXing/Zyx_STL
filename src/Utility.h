d027de94d220966949dafffaaa1e6aa7b1e42a5f
ZhengYuXing <zhengyuxing49@gmail.com>
2016-11-02 15:39:37 +0800

    add swap() function of array

--- src/Utility.h
+++ src/Utility.h
@@ -1,14 +1,29 @@
 #ifndef ZYX_UTILITY
 #define ZYX_UTILITY 
 
+#include <cstddef>
+
 namespace Zyx {
 
 template <typename T>
 void swap(T& a, T& b)
 {
-    T tmp = a;
+    T c(a);
     a = b;
-    b = tmp;
+    b = c;
+
+    // // in C++11:
+    // T c(std::move(a));
+    // a = std::move(b);
+    // b = std::move(c);
+}
+
+template <typename T, size_t N>
+void swap(T (&a)[N], T (&b)[N])
+{
+    for (size_t i = 0; i < N; i++) {
+        swap(a[i], b[i]);
+    }
 }
 
 template <typename ForwardIterator1, typename ForwardIterator2>
